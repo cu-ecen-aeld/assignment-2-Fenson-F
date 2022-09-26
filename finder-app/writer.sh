ce7631ec9f7cf3c340cb4b8fb44aa875c8505914
Fenson-F <fdfenlon@gmail.com>
2022-09-26 20:15:50 +0000

    added finder.sh and writer.sh, and updated test_validate_username.c.  Issues with previous push did not update them properly in the repo

---
+++ finder-app/writer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Error: Filename with path and file text not given."
+	exit 1
+elif [ $# -eq 1 ]
+then
+	echo "Error: Filename with path OR file text not given."
+	exit 1
+else
+	writefile=$1
+	writestr=$2
+	filedir=$(dirname $writefile)
+	filename=$(basename $writefile)
+	if [ ! -d "$filedir" ] 
+	then
+		mkdir $filedir
+		echo File directory created at "$filedir"
+	fi	
+	echo $writestr >| $writefile
+	echo File "$writefile" created
+	exit 0
+fi
+
+echo "Error: could not create file"
+exit 1 
+
